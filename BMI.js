273ef66c80000725518066213e3d31f5192bd309
ASAD <ak5963905@gmail.com>
2023-10-29 00:40:27 -0700

    pro

---
+++ BMI.js
@@ -0,0 +1,31 @@
+const form=document.querySelector("form")
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+    const height= parseInt(document.querySelector("#height").value);
+    const weight=parseInt(document.querySelector("#weight").value);
+    const cheacker=document.querySelector("#cheacker")
+   
+
+    const reasult=document.querySelector("#reasult")
+// cheak validation on height
+if(height === "" || height<0 || isNaN(height)){
+    reasult.textContent= `Enter a valid height ${height}`
+}else if(weight === "" || weight<0 || isNaN(weight)){
+    reasult.textContent= `Enter a valid weight ${weight}`
+}else{
+    const bmi=(weight / ((height*height) / 1000).toFixed(2))
+    // show the reasult
+    reasult.innerHTML=`<span>${bmi + " BMI"}</span>`
+    if(bmi==18.5){
+        cheacker.innerHTML="Under weight"
+    }else if (bmi==18.6 || cheacker==24.5){
+        cheacker.innerHTML="Normal weight"
+    }else if(bmi==30){
+        cheacker.innerHTML="Over weight"
+    }else{
+        cheacker.innerHTML="soory"
+    }
+}
+
+
+})
--- box_color_changer.js
+++ box_color_changer.js
@@ -6,17 +6,35 @@ console.log(button);
 button.addEventListener("click",function(e){
     console.log(e);
     console.log(e.target);
-    if(e.target.id==="grey"){
+    // if(e.target.id==="grey"){
+    //     body.style.backgroundColor=e.target.id
+    // }
+    // if(e.target.id==="blue"){
+    //     body.style.backgroundColor=e.target.id
+    // }
+    // if(e.target.id==="green"){
+    //     body.style.backgroundColor=e.target.id
+    // }
+    // if(e.target.id==="red"){
+    //     body.style.backgroundColor=e.target.id
+    // }
+
+    const as=2
+    switch (as) {
+        case 1:e.target.id="grey";
         body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id==="blue"){
+            break;
+        case 2:e.target.id="blue";
         body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id==="green"){
-        body.style.backgroundColor=e.target.id
-    }
-    if(e.target.id==="red"){
-        body.style.backgroundColor=e.target.id
+            break;
+            case 3:e.target.id="green";
+            body.style.backgroundColor=e.target.id
+            break;
+            case 4:e.target.id="red";
+            body.style.backgroundColor=e.target.id
+            break;
+        default:console.log("asad");
+            break;
     }
 })
 })
